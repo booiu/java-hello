524b2c0bd25d4e977e8f6704c29a719972379642
booiu <booiu@qq.com>
2022-12-12 15:55:41 +0800

    first

---
+++ src/main/java/Helloworld.java
@@ -0,0 +1,10 @@
+/**
+ * @auther:王彦伟
+ * @Wechat：AI-LIU_11
+ * @Goal:
+ */
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello World! git!");
+    }
+}
